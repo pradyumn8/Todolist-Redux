b5f430621b0e1f6423dd4fd4e46f60ddd63ab5ef
pradyumn8 <pradyumnv8@gmail.com>
2024-04-02 14:03:56 +0530

    adding todo features

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,25 +1,38 @@
-import React,{useState} from 'react'
-import { addTodo
- } from '../features/todo/todoSlice'
-
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../features/todo/todoSlice';
 
 function AddTodo() {
-  return (
-    <form> 
-        <input 
-        type="text" 
-        className='bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
-        placeholder='Enter a Todo...'
- 
-        />
-        <button
-        type='submit'
-        className='text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg'
-        >
-        Add Todo
-        </button>
-    </form>
-  )
+    const [input, setInput] = useState('');
+    const dispatch = useDispatch();
+
+    const addTodoHandler = (e) => {
+        e.preventDefault();
+        if (input.trim() !== '') { // Check if input is not empty
+            dispatch(addTodo(input));
+            setInput('');
+        } else {
+            alert('Please enter a todo before adding.'); // Remind to input if empty
+        }
+    };
+
+    return (
+        <form onSubmit={addTodoHandler} className='space-x-3 mt-12'>
+            <input
+                type="text"
+                className='bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+                placeholder='Enter a Todo...'
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            />
+            <button
+                type='submit'
+                className='text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg'
+            >
+                Add Todo
+            </button>
+        </form>
+    );
 }
 
-export default AddTodo
+export default AddTodo;
